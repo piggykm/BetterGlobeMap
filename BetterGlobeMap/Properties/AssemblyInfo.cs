235a4bfc3374d5c24bb402120006120a3d39558c
piggykm <piggykm@gmail.com>
2022-09-14 02:16:04 +0900

    # v0.0.3 add video guide

---
+++ BetterGlobeMap/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyCompany("DSPBetterMove")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyFileVersion("0.0.3")]
+[assembly: AssemblyInformationalVersion("0.0.3")]
+[assembly: AssemblyProduct("DSPBetterMove")]
+[assembly: AssemblyTitle("DSPBetterMove")]
+[assembly: AssemblyVersion("0.0.3")]
+[assembly: NeutralResourcesLanguage("0.0.3")]
